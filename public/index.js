2df9c0950c98b8c662a8a44f051b08be0db1cb81
Fabio <contessottofabio@itis-molinari.eu>
2025-02-06 19:20:31 +0100

    file

--- public/index.js
+++ public/index.js
@@ -2,25 +2,39 @@
   const inputFile = document.querySelector('#file');
   const button = document.querySelector("#button");
   const link = document.querySelector("#link");
+  const fileList = document.querySelector("#fileList");
 
-  handleSubmit = async (event) => {
+  const loadFileList = () => {
+    fetch("/filelist")
+      .then(res => res.json())
+      .then(files => {
+        fileList.innerHTML = files.map(file => `
+          <li>
+            <a href="${file}" target="_blank">${file}</a>
+          </li>
+        `).join('');
+      })
+      .catch(e => console.log(e));
+  };
+
+  const handleSubmit = (event) => {
     const formData = new FormData();
     formData.append("file", inputFile.files[0]);
-    const body = formData;
-    body.description = inputDescription.value;
     const fetchOptions = {
       method: 'post',
-      body: body
+      body: formData
     };
-    try {
-      const res = await fetch("/upload", fetchOptions);
-      const data = await res.json();
-      link.setAttribute("href", data.url);
-      link.innerText = data.url;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+    
+    fetch("/upload", fetchOptions)
+      .then(res => res.json())
+      .then(data => {
+        link.setAttribute("href", data.url);
+        link.innerText = data.url;
+        loadFileList();
+      })
+      .catch(e => console.log(e));
+  };
 
   button.onclick = handleSubmit;
+  loadFileList();
 })();
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require('http');
 const path = require('path');
+const fs = require('fs'); // Importa il modulo fs
 const app = express();
 const multer  = require('multer');
+
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, path.join(__dirname, "files"));
@@ -11,19 +13,31 @@ var storage = multer.diskStorage({
         callback(null, file.originalname);
     }
 });
-const upload = multer({ storage: storage}).single('file');
+
+const upload = multer({ storage: storage }).single('file');
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/files", express.static(path.join(__dirname, "files")));
+
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
-        
-    console.log(req.file.filename);    
-    res.json({url: "./files/" + req.file.filename});    
-  })
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        console.log(req.file.filename);
+        res.json({ url: "./files/" + req.file.filename });
+    });
+});
+
+app.get('/filelist', (req, res) => {
+    const directoryPath = path.join(__dirname, 'files');
+    fs.readdir(directoryPath, (err, files) => {
+        const fileUrls = files.map(file => `./files/${file}`);
+        res.json(fileUrls);
+    });
 });
 
 const server = http.createServer(app);
 server.listen(5600, () => {
-  console.log("- server running");
+    console.log("- server running");
 });
